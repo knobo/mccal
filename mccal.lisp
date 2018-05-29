35bbbb1c649710e3252996bf972dcdc3624323b8
Knut Olav Bøhmer <bohmer@gmail.com>
2018-05-29 13:13:54 +0200

    use vertically layout

--- mccal.lisp
+++ mccal.lisp
@@ -5,54 +5,6 @@
 (define-presentation-type date ()
   :inherit-from t)
 
-
-(defclass calendar-pane (clim-stream-pane)
-  ((date :accessor get-date :initarg :date)))
-
-(defmethod compose-space ((pane calendar-pane) &key width height)
-  (declare (ignore width height))
-  (make-space-requirement :width 1200 :height 1800))
-
-(defmethod handle-repaint ((pane calendar-pane) region)
-  (let* ((date      (get-date pane))
-         (start-day (local-time:timestamp-minimize-part date :day :timezone local-time:+gmt-zone+))
-         (end-day   (local-time:timestamp-maximize-part date :day))
-         (iterator-day (calendar:adjust-weekday start-day 1)))
-
-    (formatting-table (pane :x-spacing '(3 :character) :y-spacing '(1 :character))
-
-      ;; Draw table header
-      (surrounding-output-with-border
-          (pane :shape          :rectangle
-                :background     +light-sea-green+
-                :line-thickness 1)
-        (formatting-row (pane)
-          (dolist (wd '(" - "  m t w t f s s))
-            (formatting-cell (pane :align-x :right)
-              (with-drawing-options (pane :ink +blue+)
-                (format pane "~a" wd))))))
-
-      ;; Draw month
-      (dotimes (week (1+ (calendar:weeks end-day start-day)))
-        (formatting-row (pane)
-
-          ;; First cell is week number
-          (formatting-cell (pane :align-x :right)
-            (with-drawing-options (pane :ink +blue+)
-              (format pane "~a" (calendar:week-num iterator-day))))
-
-          ;; Draw one week per row
-          (dotimes (i 7)
-            (formatting-cell (pane :align-x :right)
-              (with-output-as-presentation (pane (local-time:clone-timestamp iterator-day) 'date)
-                (with-drawing-options (pane :ink (cond
-                                                   ((calendar:same-day date iterator-day) +magenta+)
-                                                   ((calendar:same-month date iterator-day) +black+)
-                                                   (t +gray+)))
-                  (draw-text* pane (format nil "~a" (local-time:timestamp-day iterator-day))  0 0))))
-            (local-time:adjust-timestamp! iterator-day (offset :day 1))))))))
-
-
 (define-application-frame calendar ()
   ((date :initarg :date :accessor date))
   (:panes
@@ -62,7 +14,8 @@
                 (vertically ()
                   (2/4 cal)
                   (1/4 input))))
-   (:geometry :width 357 :height 400))
+  ;; (:geometry :width 357 :height 500)
+  )
 
 (defgeneric display (frame pane)
   )
@@ -70,32 +23,112 @@
 (defmethod display ((frame calendar) pane)
   (let ((date (date frame)))
     (with-output-as-gadget (pane)
-      (horizontally (:width 337)
-        (1/3 (clim:make-pane 'push-button
-                             :label "Prev"
-                             :name "Prev"
-                             :activate-callback (lambda (&rest args)
-                                                  (defparameter *karg* args )
-                                                  (com-select-date
-                                                   (local-time:adjust-timestamp date (offset :month -1)))
-                                                  (redisplay-frame-pane frame pane))))
-        (1/3 (make-pane 'label-pane
-                        :align-x  :center
-                        :label
-                        (local-time:format-timestring nil date :format '(:long-month " " (:year 4)))))
+      (vertically ()
+        (2/10
+         (horizontally ()
+           (1/3 (clim:make-pane 'push-button
+                                :label "Prev"
+                                :name "Prev"
+                                :activate-callback (lambda (&rest args)
+                                                     (defparameter *karg* args )
+                                                     (com-select-date
+                                                      (local-time:adjust-timestamp date (offset :month -1)))
+                                                     (redisplay-frame-pane frame pane))))
+           (1/3 (make-pane 'label-pane
+                           :align-x  :center
+                           :label
+                           (local-time:format-timestring nil date :format '(:long-month " " (:year 4)))))
 
-        (1/3 (clim:make-pane 'push-button
-                             :label "Next"
-                             :activate-callback (lambda (&rest args)
-                                                  (declare (ignore args))
-                                                  (com-select-date
-                                                   (local-time:adjust-timestamp date (offset :month 1)))
-                                                  (redisplay-frame-pane frame pane))))))
+           (1/3 (clim:make-pane 'push-button
+                                :label "Next"
+                                :activate-callback (lambda (&rest args)
+                                                     (declare (ignore args))
+                                                     (com-select-date
+                                                      (local-time:adjust-timestamp date (offset :month 1)))
+                                                     (redisplay-frame-pane frame pane)))))
+         (8/10  (let* ((start-day (local-time:timestamp-minimize-part date :day :timezone local-time:+gmt-zone+))
+                       (end-day   (local-time:timestamp-maximize-part date :day))
+                       (iterator-day (calendar:adjust-weekday start-day 1)))
 
-    (fresh-line)
-    (with-output-as-gadget (pane)
-      (horizontally (:width 337)
-        (make-pane 'calendar-pane :date date)))))
+                  (formatting-table (pane :x-spacing '(3 :character) :y-spacing '(1 :character))
+
+                    ;; Draw table header
+                    (formatting-row (pane)
+                      (surrounding-output-with-border
+                          (pane :shape          :rectangle
+                                :background     +light-sea-green+
+                                :line-thickness 1)
+                        (dolist (wd '(" - "  m t w t f s s))
+                          (formatting-cell (pane :align-x :right)
+                            (with-drawing-options (pane :ink +blue+)
+                              (format pane "~a" wd))))))
+
+                    ;; Draw month
+                    (dotimes (week (1+ (calendar:weeks end-day start-day)))
+                      (formatting-row (pane)
+
+                        ;; First cell is week number
+                        (formatting-cell (pane :align-x :right)
+                          (with-drawing-options (pane :ink +blue+)
+                            (format pane "~a" (calendar:week-num iterator-day))))
+
+                        ;; Draw one week per row
+                        (dotimes (i 7)
+                          (formatting-cell (pane :align-x :right)
+                            (with-output-as-presentation (pane (local-time:clone-timestamp iterator-day) 'date)
+                              (with-drawing-options (pane :ink (cond
+                                                                     ((calendar:same-day date iterator-day) +magenta+)
+                                                                     ((calendar:same-month date iterator-day) +black+)
+                                                                     (t +gray+)))
+                                (draw-text* pane (format nil "~a" (local-time:timestamp-day iterator-day))  0 0))))
+                          (local-time:adjust-timestamp! iterator-day (offset :day 1)))))))))))
+
+#|    (fresh-line)
+    (let* ((start-day (local-time:timestamp-minimize-part date :day :timezone local-time:+gmt-zone+))
+           (end-day   (local-time:timestamp-maximize-part date :day))
+           (iterator-day (calendar:adjust-weekday start-day 1)))
+
+      (formatting-table (pane :x-spacing '(3 :character) :y-spacing '(1 :character))
+
+        ;; Draw table header
+        (formatting-row (pane)
+          (surrounding-output-with-border
+              (pane :shape          :rectangle
+                    :background     +light-sea-green+
+                    :line-thickness 1)
+            (dolist (wd '(" - "  m t w t f s s))
+              (formatting-cell (pane :align-x :right)
+                (with-drawing-options (pane :ink +blue+)
+                  (format pane "~a" wd))))))
+
+        ;; Draw month
+        (dotimes (week (1+ (calendar:weeks end-day start-day)))
+          (formatting-row (pane)
+
+            ;; First cell is week number
+            (formatting-cell (pane :align-x :right)
+              (with-drawing-options (pane :ink +blue+)
+                (format pane "~a" (calendar:week-num iterator-day))))
+
+            ;; Draw one week per row
+            (dotimes (i 7)
+              (formatting-cell (pane :align-x :right)
+                (with-output-as-presentation (pane (local-time:clone-timestamp iterator-day) 'date)
+                  (with-drawing-options (pane :ink (cond
+                                                     ((calendar:same-day date iterator-day) +magenta+)
+                                                     ((calendar:same-month date iterator-day) +black+)
+                                                     (t +gray+)))
+                    (draw-text* pane (format nil "~a" (local-time:timestamp-day iterator-day))  0 0))))
+              (local-time:adjust-timestamp! iterator-day (offset :day 1)))))
+|#
+))
+
+#|
+(make-pane 'calendar-pane
+                          :date date
+                          :display-function #'display-calendar)
+|#
+
 
 (defmethod run-calendar ()
   (run-frame-top-level (make-application-frame 'calendar :date (local-time:today))))
